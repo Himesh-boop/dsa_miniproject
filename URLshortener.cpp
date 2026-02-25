3d6aeef69666a36b1e4491fb01b39676ec977383
Himesh-boop <himeshbandari5@gmail.com>
2026-02-25 22:09:16 +0545

    url shortener using hash map is created

---
+++ URLshortener.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include "base62.cpp"
+
+using namespace std;
+
+class URLshortener{
+    private:
+        unordered_map<string, string> longToShort;
+        unordered_map<string, string> shortToLong;
+        long long counter;
+        
+        string BASE62 =
+        "abcdefghijklmnopqrstuvwxyz"
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "0123456789";
+    
+    public:
+        URLshortener() {
+            counter = 1;
+        }
+        
+        string shortenURL(string longURL){
+            if(longToShort.find(longURL)!=longToShort.end()){
+                return longToShort[longURL];
+            }
+            string shortCode = encodeBase62(counter);
+            shortToLong[shortCode] = longURL;
+            longToShort[longURL] = shortCode;
+
+            return shortCode;
+        }
+        string getOriginalURL(string shortCode) {
+            if (shortToLong.find(shortCode) != shortToLong.end()) {
+                return shortToLong[shortCode];
+            }
+            return "URL Not Found";
+        }
+};
